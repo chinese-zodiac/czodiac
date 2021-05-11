c32f825066f3ca0474faf482f765e620b1d3d14f
Plastic Digits <plasticdigits@protonmail.com>
2021-05-11 02:52:21 -0700

    Add background lines

--- packages/ui/src/pages/Home/index.js
+++ packages/ui/src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import Header from "../../components/Header";
+import BackgroundLines from "../../components/BackgroundLines";
+import { Box, Button, LightMode, DarkMode, Link } from "@chakra-ui/react";
+import { NavLink } from "react-router-dom";
+import "./index.scss";
 
 function Home() {
   return (<>
-    <Header />
-    <main>
-        Home Page
-    </main>
+    <BackgroundLines />
+      <Header />
+    <LightMode>
+      <Box as="main" className="both-center home-page">
+          <NavLink to="sale">
+            <Button colorScheme="red" >Liquidity Sale</Button>
+          </NavLink>
+          <Button isDisabled={true} colorScheme="red" >Swap (coming soon)</Button>
+          <Link href="https://czodiac.com" isExternal>
+            <Button colorScheme="orange" variant="outline" >Information</Button>
+          </Link >
+          <br/><br/>
+      </Box>
+    </LightMode>
 </>);
 }
 
