30bca2936f355cc03bdeee56a41e346e034d72c8
Plastic Digits <plasticdigits@protonmail.com>
2021-05-11 22:31:46 -0700

    Add layout for liquidity sale page

--- packages/ui/src/pages/Sale/index.js
+++ packages/ui/src/pages/Sale/index.js
@@ -2,30 +2,36 @@ import React from "react";
 import { Box, Heading, Icon, Text, Link, Button, NumberInput, NumberInputField, NumberInputStepper, NumberIncrementStepper, NumberDecrementStepper } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import useLockedSale from "../../hooks/useLockedSale";
-import { FiExternalLink } from "react-icons/fi";
+import { FiExternalLink, FiCheck } from "react-icons/fi";
+import { SimpleGrid } from "@chakra-ui/react"
 import "./index.scss";
+
 
 function Sale() {
   const format = (val) => `${val} BNB`;
   const parse = (val) => val.replace(/^\$/, "");
   const [value, setValue] = React.useState("1.53");
+
+  const maxBNB = 2;
+
   return (<>
     <Header />
     <Box as="main" className="both-center sale-page">
-      <Heading>Initial Liquidity Sale</Heading>
+      <Heading mt="100px">Initial Liquidity Sale</Heading>
       <Text className="explanation">
       The Sale runs from the start countdown to the end countdown.
-      There is a limit of 5 BNB per buyer and a minimum of 0.1 BNB.
+      There is a limit of {maxBNB} BNB per buyer and a minimum of 0.1 BNB.
       The Sale can only reach a maximum of 100 BNB.
       Tokens are locked for 1 week and auto distributed.
       0% of tokens to team. Fair Launch!
       100% of BNB will be locked as liquidity.
-      Whitelisting and more information on <Link isExternal color="orange.700" href="https://t.me/CZodiacofficial">Telegram <Icon as={FiExternalLink} /></Link>. 
+      Whitelisting and more information on <Link isExternal color="orange.700" href="https://t.me/CZodiacofficial">Telegram <Icon as={FiExternalLink} /></Link>.<br/>
+      <b> USA citizens, residents, agents etc are excluded.</b>
       </Text>
       <NumberInput 
         onChange={(valueString) => setValue(parse(valueString))}
         value={format(value)}
-        className="bnbInput" defaultValue={1} precision={1} step={0.1} min={0.1} max={5}>
+        className="bnbInput" defaultValue={1} precision={1} step={0.1} min={0.1} max={maxBNB}>
         <NumberInputField />
         <NumberInputStepper>
           <NumberIncrementStepper />
@@ -33,6 +39,41 @@ function Sale() {
         </NumberInputStepper>
       </NumberInput>
       <Button className="purchaseButton" >Purchase</Button>
+      <hr />
+      <Heading as="h2" size="md">Your Stats</Heading>
+      <SimpleGrid className="stats" columns={2} spacing={1}>
+        <Text>Whitelist Status:</Text>
+        <Text><Icon color="green.700" as={FiCheck} /></Text>
+        <Text>Spendings:</Text>
+        <Text>{0} BNB</Text>
+        <Text>OxZodiac:</Text>
+        <Text>{0} OxZodiac</Text>
+      </SimpleGrid>
+      <hr />
+      <Heading as="h2" size="md">Sale Stats</Heading>
+      <SimpleGrid className="stats" columns={2} spacing={1}>
+        <Text>Total Buyers:</Text>
+        <Text>{0}</Text>
+        <Text>Total Purchases:</Text>
+        <Text>{0} BNB</Text>
+        <Text>Rate:</Text>
+        <Text>20B OxZodiac/BNB</Text>
+        <Text>Opening Date:</Text>
+        <Text>TBD</Text>
+        <Text>Closing Date:</Text>
+        <Text>TBD</Text>
+        <Text>Unlock Date:</Text>
+        <Text>TBD</Text>
+        <Text>Sale Size:</Text>
+        <Text>2T OxZodiac</Text>
+        <Text>Sale Cap:</Text>
+        <Text>100 BNB</Text>     
+        <Text>Token:</Text>
+        <Link isExternal color="orange.700" 
+          href="https://etherscan.io">0x0000...
+        </Link>
+      </SimpleGrid>
+      <hr />
     </Box>
   </>);
 }
