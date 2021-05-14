395d773da57f85dad1bfa279936f317e338b9003
Plastic Digits <plasticdigits@protonmail.com>
2021-05-14 04:11:34 -0700

    Add tooltip for network switcher

--- packages/ui/src/components/Header/index.js
+++ packages/ui/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEthers } from "@pdusedapp/core";
-import { useColorMode, Button, Heading, Text, Box, Icon } from "@chakra-ui/react";
-import { FiSun, FiMoon, FiCornerDownRight } from "react-icons/fi";
+import { useColorMode, Button, Heading, Text, Box, Icon  } from "@chakra-ui/react";
+import { FiSun, FiMoon, FiCornerDownRight, FiHelpCircle } from "react-icons/fi";
 import { CHAIN_LABELS, CHAINS } from "../../constants";
 import { NavLink } from "react-router-dom";
 import { addChainToMetaMask } from "../../utils/metamask";
@@ -32,16 +32,28 @@ function Header() {
                 <Button className={"color-mode"} onClick={toggleColorMode}>
                     {colorMode === "light" ? (<Icon as={FiMoon} />) : (<Icon as={FiSun} />)}
                 </Button>
-                {(!!chainId && (chainId != CHAINS.BSC || !account)) && (
+                {(!!chainId && (chainId != CHAINS.BSC || !account)) && (<>
                     <Button className="network-picker" onClick={()=>addChainToMetaMask(CHAINS.BSC)}>
                         <Icon as={FiCornerDownRight} />BSC
+                        <Box className="network-tooltip">
+                            <Icon className="network-tooltip-icon" as={FiHelpCircle} />
+                            <Text bg="gray.300" color="gray.900"  className="network-tooltip-text">
+                            Switches to BSC in some wallets. On others, you will need to switch manually.
+                            </Text>
+                        </Box>
                     </Button>
-                )}
-                {(!!chainId && chainId == CHAINS.BSC && !!account) && (
+                </>)}
+                {(!!chainId && chainId == CHAINS.BSC && !!account) && (<>
                     <Button className="network-picker" onClick={()=>addChainToMetaMask(CHAINS.BSCTestnet)}>
                         <Icon as={FiCornerDownRight} />BSCT
+                        <Box className="network-tooltip">
+                            <Icon className="network-tooltip-icon" as={FiHelpCircle} />
+                            <Text bg="gray.300" color="gray.900"  className="network-tooltip-text">
+                            Test the app on BSCT. Not supported by all wallets.
+                            </Text>
+                        </Box>
                     </Button>
-                )}
+                </>)}
             </div>
             <Text className="version">v0.0.6</Text>
         </header>
