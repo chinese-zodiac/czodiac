78c0e7ee242f5681c75febd7520da4b931bbb468
Plastic Digits <plasticdigits@protonmail.com>
2021-05-18 18:06:33 -0700

    Update images

--- packages/main/src/sections/About/index.js
+++ packages/main/src/sections/About/index.js
@@ -22,7 +22,7 @@ function About() {
         <Text color="gray.900" fontWeight="bold" >
         Since we are in 2021, we will start with OX, and after each month instead of yearly swap we will do a token swap every 30days to the next Zodiac decreasing the supply 8:1 until we finish the 12 Chinese Zodiac Sign and will have a fixed supply of CZodiac Token.
         </Text>
-        <Parallax x={["-8%","8%"]} className="header-full">
+        <Parallax x={["-8%","8%"]} className="header-thin">
           <Image src="./header_thin.png" borderBottom="solid 4px" borderColor="gray.900" />
         </Parallax>
         <Heading className=""
