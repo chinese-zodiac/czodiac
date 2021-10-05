3beead031ef6fd4a5eda4b78dc62e864c48682e9
Plastic Digits <plasticdigits@protonmail.com>
2021-10-05 02:44:57 -0700

    Update block for hardhat fork

--- packages/sc/hardhat.config.js
+++ packages/sc/hardhat.config.js
@@ -32,7 +32,7 @@ module.exports = {
     hardhat: {
       forking: {
         url: `https://speedy-nodes-nyc.moralis.io/${networkConfig.moralisRpcKey}/bsc/mainnet/archive`,
-        blockNumber:  9227900
+        blockNumber:  11504400
       }
     },
     rinkeby: {
