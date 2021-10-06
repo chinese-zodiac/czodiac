ff291701ba11bcf372e05014d6bde6de6186883a
anthowen <anthowen@users.noreply.github.com>
2021-10-05 21:52:51 -0400

    Duplicate CZVaults

--- packages/ui/src/components/CZVaultList/index.js
+++ packages/ui/src/components/CZVaultList/index.js
@@ -1,98 +1,172 @@
 import React, { useState } from "react";
-import { Box, Button, LightMode, Icon, Link, Text, Heading, Image,
-Tabs, TabList, TabPanels, Tab, TabPanel, SimpleGrid, Divider, Slider, SliderTrack, SliderFilledTrack, SliderThumb } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  LightMode,
+  Icon,
+  Link,
+  Text,
+  Heading,
+  Image,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  SimpleGrid,
+  Divider,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react";
 import { FiExternalLink } from "react-icons/fi";
 import { useEthers } from "@pdusedapp/core";
-import { CZODIAC_ADDRESSES, BUSD_ADDRESSES, WETH_ADDRESSES, CZFARM_ADDRESSES } from "../../constants";
+import {
+  CZODIAC_ADDRESSES,
+  BUSD_ADDRESSES,
+  WETH_ADDRESSES,
+  CZFARM_ADDRESSES,
+} from "../../constants";
 import { Contract, utils, BigNumber, constants } from "ethers";
 import useBUSDPrice from "../../hooks/useBUSDPrice";
-import useCZPools from "../../hooks/useCZPools";
-import {weiToFixed, weiToShortString, toShortString} from "../../utils/bnDisplay";
+import useCZVaults from "../../hooks/useCZVaults";
+import {
+  weiToFixed,
+  weiToShortString,
+  toShortString,
+} from "../../utils/bnDisplay";
 import "./index.scss";
 import CZPool from "../CZPool";
 
-const tokenLink = (address, name)=>{return (<Link style={{fontWeight:"bold",textDecoration:"underline"}} isExternal href={`https://bscscan.com/token/${address}`}>{name}</Link>)}
-const czfarmLink = ()=>tokenLink("0x7c1608C004F20c3520f70b924E2BfeF092dA0043","$CZF");
+const tokenLink = (address, name) => {
+  return (
+    <Link
+      style={{ fontWeight: "bold", textDecoration: "underline" }}
+      isExternal
+      href={`https://bscscan.com/token/${address}`}
+    >
+      {name}
+    </Link>
+  );
+};
+const czfarmLink = () =>
+  tokenLink("0x7c1608C004F20c3520f70b924E2BfeF092dA0043", "$CZF");
 
 function CZVaultList() {
-  const {chainId} = useEthers();
-  const {
-    pools
-  } = useCZPools();
+  const { chainId } = useEthers();
+  const { pools } = useCZVaults();
 
-  const [basisPoints, setBasisPoints] = useState(pools.map((p)=>10000))
+  const [basisPoints, setBasisPoints] = useState(pools.map((p) => 10000));
 
-  const [currentDate] = useState(new Date())
+  const [currentDate] = useState(new Date());
 
-  const displayPools = (filter,pools) => {
-    return (!!pools && pools.length > 0) && (<>
-      {pools.map((pool, index)=>{
-        if(!filter(pool)) return;
-        return(<Box key={"pid-"+pool.name} border="solid 1px" borderRadius="5px" m="0px" mb="20px" p="20px" fontSize={{base:"x-small",md:"md"}}>
-           <CZPool 
-              sendDeposit={pool.sendDeposit}
-              sendWithdraw={pool.sendWithdraw}
-              rewardAddress={pool.rewardAddress}
-              rewardDecimals={pool.rewardDecimals}
-              aprBasisPoints={pool.aprBasisPoints}
-              rewardPerDay={pool.rewardPerDay}
-              usdValue={pool.usdValue}
-              usdPerDay={pool.usdPerDay}
-              timeStart={pool.timeStart}
-              timeEnd={pool.timeEnd}
-              user={pool.user}
-              name={pool.name}
-              logo={pool.logo}
-            />
-        </Box>
-      )})}</>)
-  }
+  const displayPools = (filter, pools) => {
+    return (
+      !!pools &&
+      pools.length > 0 && (
+        <>
+          {pools.map((pool, index) => {
+            if (!filter(pool)) return;
+            return (
+              <Box
+                key={"pid-" + pool.name}
+                border="solid 1px"
+                borderRadius="5px"
+                m="0px"
+                mb="20px"
+                p="20px"
+                fontSize={{ base: "x-small", md: "md" }}
+              >
+                <CZPool
+                  sendDeposit={pool.sendDeposit}
+                  sendWithdraw={pool.sendWithdraw}
+                  rewardAddress={pool.rewardAddress}
+                  rewardDecimals={pool.rewardDecimals}
+                  aprBasisPoints={pool.aprBasisPoints}
+                  rewardPerDay={pool.rewardPerDay}
+                  usdValue={pool.usdValue}
+                  usdPerDay={pool.usdPerDay}
+                  timeStart={pool.timeStart}
+                  timeEnd={pool.timeEnd}
+                  user={pool.user}
+                  name={pool.name}
+                  logo={pool.logo}
+                />
+              </Box>
+            );
+          })}
+        </>
+      )
+    );
+  };
 
+  return (
+    <>
+      <p>Earn partnered tokens by pooling {czfarmLink()}.</p>
+      <br />
+      <Tabs>
+        <TabList variant="enclosed">
+          <Tab>Active</Tab>
+          <Tab>Launching</Tab>
+          <Tab>Expired</Tab>
+        </TabList>
 
-  return (<>
-    <p>Earn partnered tokens by pooling {czfarmLink()}.</p>
-    <br/>
-    <Tabs>
-      <TabList variant="enclosed">
-        <Tab>Active</Tab>
-        <Tab>Launching</Tab>
-        <Tab>Expired</Tab>
-      </TabList>
-
-      <TabPanels>
-        <TabPanel p="0px" pt="20px">
-          {displayPools(
-            pool=>(pool.timeStart <= currentDate && pool.timeEnd >= currentDate),
-            pools
-          )}
-        </TabPanel>
-        <TabPanel p="0px" pt="20px">
-          {displayPools(
-            pool=>(pool.timeStart > currentDate),
-            pools
-          )}
-        </TabPanel>
-        <TabPanel p="0px" pt="20px">
-          {displayPools(
-            pool=>(pool.timeEnd < currentDate),
-            pools
-          )}
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-    <Text fontWeight="bold">Pools Totals</Text>
-    {(!!pools && pools.length > 0) ? (
-    <SimpleGrid columns="2" spacing="1" >
-      <Text textAlign="right">Pool Count:</Text><Text textAlign="left">{pools.length}</Text>
-      <Text textAlign="right">Active USD/Day Rewards:</Text><Text textAlign="left">
-        ${weiToShortString(pools.reduce((prev,curr,index,pools)=>((pools[index].timeStart <= currentDate && pools[index].timeEnd >= currentDate) ? prev.add(pools[index].usdPerDay) : prev),BigNumber.from("0")),2)}</Text>
-      <Text textAlign="right">Total Value Locked:</Text><Text textAlign="left">${weiToShortString(pools.reduce((prev,curr,index,pools)=>prev.add(pools[index].usdValue),BigNumber.from("0")),2)}</Text>
-    </SimpleGrid>) : (
-      <Box>Loading pools...</Box>
-    )}
-    <br/>
-    <Divider />
-  </>)
+        <TabPanels>
+          <TabPanel p="0px" pt="20px">
+            {displayPools(
+              (pool) =>
+                pool.timeStart <= currentDate && pool.timeEnd >= currentDate,
+              pools
+            )}
+          </TabPanel>
+          <TabPanel p="0px" pt="20px">
+            {displayPools((pool) => pool.timeStart > currentDate, pools)}
+          </TabPanel>
+          <TabPanel p="0px" pt="20px">
+            {displayPools((pool) => pool.timeEnd < currentDate, pools)}
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Text fontWeight="bold">Pools Totals</Text>
+      {!!pools && pools.length > 0 ? (
+        <SimpleGrid columns="2" spacing="1">
+          <Text textAlign="right">Pool Count:</Text>
+          <Text textAlign="left">{pools.length}</Text>
+          <Text textAlign="right">Active USD/Day Rewards:</Text>
+          <Text textAlign="left">
+            $
+            {weiToShortString(
+              pools.reduce(
+                (prev, curr, index, pools) =>
+                  pools[index].timeStart <= currentDate &&
+                  pools[index].timeEnd >= currentDate
+                    ? prev.add(pools[index].usdPerDay)
+                    : prev,
+                BigNumber.from("0")
+              ),
+              2
+            )}
+          </Text>
+          <Text textAlign="right">Total Value Locked:</Text>
+          <Text textAlign="left">
+            $
+            {weiToShortString(
+              pools.reduce(
+                (prev, curr, index, pools) => prev.add(pools[index].usdValue),
+                BigNumber.from("0")
+              ),
+              2
+            )}
+          </Text>
+        </SimpleGrid>
+      ) : (
+        <Box>Loading pools...</Box>
+      )}
+      <br />
+      <Divider />
+    </>
+  );
 }
 
 export default CZVaultList;
---
+++ packages/ui/src/hooks/useCZVaults.js
@@ -0,0 +1,200 @@
+import { useEffect, useState } from "react";
+import { useEthers, useContractCalls, useContractFunction, useBlockNumber } from "@pdusedapp/core";
+import { CZFARMPOOLS, CZFARM_ADDRESSES } from "../constants";
+import { Contract, utils, BigNumber, constants } from "ethers";
+import useDeepCompareEffect from "../utils/useDeepCompareEffect";
+import useBUSDPrice from "./useBUSDPrice";
+import useBUSDPriceMulti from "./useBUSDPriceMulti";
+import czFarmPool from "../abi/CZFarmPool.json";
+import ierc20 from "../abi/ierc20.json";
+import { parseEther } from "@ethersproject/units";
+const {Interface} = utils;
+
+const weiFactor = BigNumber.from("10").pow(BigNumber.from("18"));
+
+function useCZVaults() {
+  const pool = {
+    timestampStart: null,
+    timestampEnd: null,
+    rewardPerSecond: null,
+    totalAmount: null,
+    totalAmountUSD: null,
+    aprBasisPoints: null,
+    userInfo: {
+      amount: null,
+      amountUSD: null,
+      pendingReward: null
+    }
+  }
+  const { account, chainId, library } = useEthers();
+  
+  const sendWithdrawForPool = async (poolAddress,wad) => {
+    if(!account || !library || !poolAddress) return;
+    const poolContract = (new Contract(poolAddress, czFarmPoolInterface, library)).connect(library.getSigner());
+    try{
+      await poolContract.withdraw(wad);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  const sendDepositForPool = async (poolAddress,wad) => {
+    if(!account || !library || !poolAddress) return;
+    const poolContract = (new Contract(poolAddress, czFarmPoolInterface, library)).connect(library.getSigner());
+    try{
+      await poolContract.deposit(wad);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  const czfBusdPrice = useBUSDPrice(CZFARM_ADDRESSES[chainId]);
+  const rewardBusdPrices = useBUSDPriceMulti(!!CZFARMPOOLS[chainId] ? CZFARMPOOLS[chainId].map((p)=>p.rewardAddress) : []);
+
+  const ierc20Interface = new Interface(ierc20);
+  const czFarmPoolInterface = new Interface(czFarmPool);
+  const [czFarmPoolContracts, setCzFarmPoolContracts] = useState(!!CZFARMPOOLS[chainId] ? CZFARMPOOLS[chainId].map((p)=>new Contract(p.address, czFarmPoolInterface)) : []);
+
+  const [pools, setPools] = useState([]);
+  const [calls, setCalls] = useState([]);
+  const callResults = useContractCalls(calls) ?? [];
+
+  useEffect(()=>{
+    const newCalls = [];
+    if(!CZFARMPOOLS[chainId]) {
+      setCalls(newCalls)
+      return;
+    }
+    CZFARMPOOLS[chainId].forEach((p) => {
+      let ca = p.address;
+      newCalls.push({
+        abi:czFarmPoolInterface,
+        address:ca,
+        method:'timestampStart'
+      });
+      newCalls.push({
+        abi:czFarmPoolInterface,
+        address:ca,
+        method:'timestampEnd'
+      });
+      newCalls.push({
+        abi:czFarmPoolInterface,
+        address:ca,
+        method:'rewardPerSecond'
+      });
+      newCalls.push({
+        abi:ierc20Interface,
+        address:CZFARM_ADDRESSES[chainId],
+        method:'balanceOf',
+        args: [ca]
+      });
+      if(!!account){
+        newCalls.push({
+          abi:czFarmPoolInterface,
+          address:ca,
+          method:'userInfo',
+          args: [account]
+        });
+        newCalls.push({
+          abi:czFarmPoolInterface,
+          address:ca,
+          method:'pendingReward',
+          args: [account]
+        });
+        newCalls.push({
+          abi:ierc20Interface,
+          address:CZFARM_ADDRESSES[chainId],
+          method:'balanceOf',
+          args: [account]
+        });
+      }      
+    });
+    setCalls(newCalls);
+  },[account,chainId])
+
+  useDeepCompareEffect(()=>{
+    let newPools = [];
+    if(!callResults || callResults.length === 0 || !callResults[0] || !CZFARMPOOLS[chainId] || !czfBusdPrice) {
+        return;
+    }
+    CZFARMPOOLS[chainId].forEach((p, index) => {
+      let l = 7;
+      if(!account) l = 4;
+      let o = l*index
+
+      p.sendDeposit = (wad) => sendDepositForPool(p.address,wad);
+      p.sendWithdraw = (wad) => sendWithdrawForPool(p.address,wad);
+
+      p.timeStart = new Date(callResults[0+o][0].toNumber()*1000);
+      p.timeEnd = new Date(callResults[1+o][0].toNumber()*1000);
+      p.rewardPerSecond = callResults[2+o][0]
+      p.czfBal = callResults[3+o][0]
+
+      p.usdValue = p.czfBal.mul(czfBusdPrice).div(weiFactor);
+      p.rewardPerDay = p.rewardPerSecond.mul(BigNumber.from("86400"));
+      if(!!rewardBusdPrices[index]){
+        p.usdPerDay = p.rewardPerDay.mul(rewardBusdPrices[index]).div(weiFactor);
+      } else {
+        p.usdPerDay = BigNumber.from("0");
+      }
+
+      //Fixes bug where TVL includes the CZF rewards in CZF->CZF pool
+      let tvlOffset = BigNumber.from("0")
+      if(p.rewardAddress == "0x7c1608C004F20c3520f70b924E2BfeF092dA0043" && p.usdPerDay.gt(BigNumber.from("0"))) {
+        let seconds = 0;
+        if(new Date() >= p.timeStart && new Date() <= p.timeEnd) {
+          seconds = Math.floor((p.timeEnd - new Date()) / 1000);
+        } else if(new Date() < p.timeStart) {
+          seconds = Math.floor((p.timeEnd - p.timeStart) / 1000);
+        }
+        tvlOffset = p.usdPerDay.mul(BigNumber.from(seconds.toString()).div(BigNumber.from("86400")));
+        p.usdValue = p.usdValue.sub(tvlOffset);
+      }
+
+      if(p.usdValue.gt(BigNumber.from("0"))) {
+        p.aprBasisPoints = p.usdPerDay.mul(BigNumber.from("365")).mul(BigNumber.from("10000")).div(p.usdValue);
+      } else {
+        p.aprBasisPoints = BigNumber.from("0");
+      }
+
+      if(p.usdValue.gt(BigNumber.from("0"))) {
+        p.aprBasisPoints = p.usdPerDay.mul(BigNumber.from("365")).mul(BigNumber.from("10000")).div(p.usdValue.add(tvlOffset));
+      } else {
+        p.aprBasisPoints = BigNumber.from("0");
+      }
+
+       if(p.rewardAddress == "0x7c1608C004F20c3520f70b924E2BfeF092dA0043" && p.usdPerDay.gt(BigNumber.from("0"))) {
+        p.usdValue = p.usdValue.add(
+          parseEther("288385966").mul(czfBusdPrice).div(weiFactor)
+        );
+       }
+
+
+
+      if(!!account && !!callResults[4+o]){
+        p.user = {}
+        p.user.czfStaked = callResults[4+o][0];
+        p.user.rewardPending = callResults[5+o][0];
+        p.user.czfBal = callResults[6+o][0];
+
+        p.user.czfStakedUsd = p.user.czfStaked.mul(czfBusdPrice).div(weiFactor);
+        p.user.czfBalUsd = p.user.czfBal.mul(czfBusdPrice).div(weiFactor);
+        if(p.czfBal > 0) {
+          p.user.rewardPerDay = p.user.czfStaked.mul(p.rewardPerDay).div(p.czfBal);
+        } else {
+          p.user.rewardPerDay = BigNumber.from("0")
+        }
+        
+      }
+      newPools.push(p)
+    });
+    setPools(newPools);
+  },[callResults,czfBusdPrice,rewardBusdPrices])
+
+
+  return {
+    pools
+  }
+
+}
+
+export default useCZVaults;
