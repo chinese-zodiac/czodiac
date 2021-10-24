b171a0d82eb0cf2580e5cd77df841b4ae57f52e6
Plastic Digits <plasticdigits@protonmail.com>
2021-10-24 03:04:28 -0700

    add whitelisting, max per user

--- packages/sc/contracts/CZFarmV2.sol
+++ packages/sc/contracts/CZFarmV2.sol
@@ -9,23 +9,27 @@ import "./CZFarm.sol";
 
 contract CZFarmV2 is Ownable {
     using SafeERC20 for IERC20;
-    CZFarm czf;
-    IERC20 asset;
+    CZFarm public czf;
+    IERC20 public asset;
 
-    mapping(address => uint256) depositorAsset;
-    uint256 totalAsset;
-    uint256 maxAsset;
-    uint256 czfPerAsset;
+    mapping(address => uint256) public depositorAsset;
+    mapping(address => bool) public whitelist;
+    bool public isWhitelistRequired;
+    uint256 public totalAsset;
+    uint256 public maxAsset;
+    uint256 public maxPerAccount;
+    uint256 public czfPerAsset;
 
-    uint256 openEpoch;
-    uint256 closeEpoch;
-    uint256 vestEpoch;
+    uint256 public openEpoch;
+    uint256 public closeEpoch;
+    uint256 public vestEpoch;
 
     constructor(
         CZFarm _czf,
         IERC20 _asset,
         uint256 _czfPerAsset,
         uint256 _maxAsset,
+        uint256 _maxPerAccount,
         uint256 _openEpoch,
         uint256 _closeEpoch,
         uint256 _vestEpoch
@@ -33,10 +37,16 @@ contract CZFarmV2 is Ownable {
         czf = _czf;
         asset = _asset;
         setCzfPerAsset(_czfPerAsset);
+        setMaxPerAccount(_maxPerAccount);
         setMaxAsset(_maxAsset);
         setOpenEpoch(_openEpoch);
         setCloseEpoch(_closeEpoch);
         setVestEpoch(_vestEpoch);
+    }
+
+    function isWhitelisted(address _account) public view returns (bool) {
+        if (!isWhitelistRequired) return true;
+        return whitelist[_account];
     }
 
     //Returns true if the farm is open to deposits.
@@ -52,6 +62,10 @@ contract CZFarmV2 is Ownable {
     //Contract must be approved for asset
     function deposit(uint256 _wad, address _to) external {
         require(isOpen(), "CZFarmV2: Deposit not open");
+        require(
+            isWhitelisted(_to) || isWhitelisted(msg.sender),
+            "CZFarmV2: Account not whitelisted"
+        );
         depositorAsset[_to] += _wad;
         totalAsset += _wad;
         require(totalAsset <= maxAsset, "CZFarmV2: Maximum assets deposited");
@@ -92,5 +106,25 @@ contract CZFarmV2 is Ownable {
 
     function setVestEpoch(uint256 _to) public onlyOwner {
         vestEpoch = _to;
+    }
+
+    function whitelistAccounts(address[] calldata accounts) public onlyOwner {
+        for (uint256 i; i < accounts.length; i++) {
+            whitelist[accounts[i]] = true;
+        }
+    }
+
+    function unwhitelistAccounts(address[] calldata accounts) public onlyOwner {
+        for (uint256 i; i < accounts.length; i++) {
+            whitelist[accounts[i]] = false;
+        }
+    }
+
+    function setIsWhitelistRequired(bool _to) public onlyOwner {
+        isWhitelistRequired = _to;
+    }
+
+    function setMaxPerAccount(uint256 _to) public onlyOwner {
+        maxPerAccount = _to;
     }
 }
