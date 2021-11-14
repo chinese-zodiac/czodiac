3104cc8148ae5f1fd217964e8b499d60f7ed2e24
Plastic Digits <plasticdigits@protonmail.com>
2021-11-14 03:02:49 -0800

    Add ExoticMaster pricing and deposits. Add todo

--- packages/sc/contracts/ExoticMaster.sol
+++ packages/sc/contracts/ExoticMaster.sol
@@ -4,49 +4,73 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Checkpoints.sol";
-import "./interfaces/IExoticMaster.sol";
-import "./interfaces/IExoticAuction.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./ExoticVesting.sol";
 
-contract ExoticMaster is IExoticMaster {
-    using Checkpoints for Checkpoints.Checkpoint[];
+contract ExoticMaster {
+    using SafeERC20 for IERC20;
 
-    uint32 public override roundDuration;
-    uint32 public override vestDuration;
-    uint16 public override emissionBasis;
+    uint16 maxPurchaseBasis;
+    uint16 minusFactor;
+    uint16 hourlyIncreaseBasis;
 
-    uint32 public firstRoundStartEpoch;
+    IERC20 asset;
+    ExoticVesting exoticVesting;
+    address treasury;
 
-    function getRoundEpochs(uint256 _roundID)
-        external
-        view
-        override
-        returns (
-            uint32 startEpoch,
-            uint32 endEpoch,
-            uint32 vestEpoch
-        )
-    {
-        startEpoch = firstRoundStartEpoch + roundDuration * uint32(_roundID);
-        endEpoch = startEpoch + roundDuration;
-        vestEpoch = endEpoch + vestDuration;
+    struct Farm {
+        uint224 startPriceWad;
+        uint32 lastPurchaseEpoch;
+        IERC20 lp;
     }
 
-    function getRoundReward(uint256 _roundID)
-        external
-        view
-        override
-        returns (uint112)
-    {
-        //TODO: getroundreward must include voting and checkpoints
-        //return roundRewards[_roundID];
-    }
+    Farm[] farms;
 
-    function getCurrentRoundID() external view override returns (uint256) {
+    function deposit(
+        address _for,
+        uint256 _id,
+        uint256 _lpWad,
+        uint256 _minPrice
+    ) external {
+        Farm storage farm = farms[_id];
         require(
-            block.timestamp >= firstRoundStartEpoch,
-            "ExoticMaster: First round not yet started"
+            address(0) != address(farm.lp),
+            "ExoticMaster: Farm does not exist."
         );
-        return (block.timestamp - firstRoundStartEpoch) / roundDuration;
+
+        uint256 price = getPrice(_id);
+        require(
+            price < _minPrice,
+            "ExoticMaster: Price decreased below min price."
+        );
+        uint256 rewardAmt = (price * _lpWad) / 1 ether;
+        uint256 assetTotal = asset.balanceOf(address(this));
+        require(
+            (rewardAmt * maxPurchaseBasis) / 10000 <= assetTotal,
+            "ExoticMaster: Deposit too large"
+        );
+
+        farm.lastPurchaseEpoch = uint32(block.timestamp);
+        farm.startPriceWad = uint224(
+            price * ((assetTotal - rewardAmt * minusFactor) / assetTotal)
+        );
+
+        farm.lp.transferFrom(msg.sender, treasury, _lpWad);
+        exoticVesting.addVest(
+            _for,
+            uint112(rewardAmt),
+            uint32(block.timestamp),
+            uint32(365 days)
+        );
     }
+
+    function getPrice(uint256 _id) public view returns (uint256) {
+        Farm storage farm = farms[_id];
+        return
+            farm.startPriceWad *
+            (((block.timestamp - farm.lastPurchaseEpoch) *
+                (10000 + hourlyIncreaseBasis)) / (1 hours * 10000));
+    }
+
+    //TODO: owner methods to change variables, add farms, constructor
 }
