0c8dcfcd0eed8d380e4facf8a5de853e9f314d75
Plastic Digits <plasticdigits@protonmail.com>
2021-11-23 21:31:34 -0800

    Update ExoticMaster to use treasury and chrono vesting

--- packages/sc/contracts/ExoticMaster.sol
+++ packages/sc/contracts/ExoticMaster.sol
@@ -1,88 +1,188 @@
 // SPDX-License-Identifier: GPL-3.0
 // Authored by Plastic Digits
-// Credit to Olympus DAO
+// Credit to Olympus Dao
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "./ChronoVesting.sol";
+import "./CZFarm.sol";
 
-contract ExoticMaster {
+contract ExoticMaster is AccessControlEnumerable {
     using SafeERC20 for IERC20;
+    bytes32 public constant EXOTIC_PRICER = keccak256("EXOTIC_PRICER");
+    bytes32 public constant EXOTIC_LORD = keccak256("EXOTIC_LORD");
 
-    uint16 maxPurchaseBasis;
-    uint16 minusFactor;
-    uint16 hourlyIncreaseBasis;
+    uint112 public baseEmissionRate;
+    uint112 public currentEmissionRate;
+    address public treasury;
 
-    IERC20 asset;
-    ChronoVesting exoticVesting;
-    address treasury;
-
-    struct Farm {
-        uint112 startPriceWad;
-        uint112 votePower;
-        uint32 lastPurchaseEpoch;
-        IERC20 lp;
+    CZFarm public czf;
+    struct ExoticFarm {
+        uint112 czfPerAssetWad;
+        uint32 rateBasis;
+        IERC20 asset;
+        ChronoVesting chronoVesting;
     }
-    Farm[] farms;
+    ExoticFarm[] public exoticFarms;
 
-    struct Voter {
-        uint112 totalVotePower;
-        mapping(uint256 => uint112) allocation;
-    }
-    uint112 totalVotePower;
-    mapping(address => Voter) voters;
-
-    function deposit(
-        address _for,
-        uint256 _id,
-        uint256 _lpWad,
-        uint256 _minPrice
-    ) external {
-        Farm storage farm = farms[_id];
-        require(
-            address(0) != address(farm.lp),
-            "ExoticMaster: Farm does not exist."
-        );
-
-        uint256 price = getPrice(_id);
-        require(
-            price < _minPrice,
-            "ExoticMaster: Price decreased below min price."
-        );
-        uint256 rewardAmt = (price * _lpWad) / 1 ether;
-        uint256 assetTotal = asset.balanceOf(address(this));
-        require(
-            (rewardAmt * maxPurchaseBasis) / 10000 <= assetTotal,
-            "ExoticMaster: Deposit too large"
-        );
-
-        farm.lastPurchaseEpoch = uint32(block.timestamp);
-        farm.startPriceWad = uint112(
-            price * ((assetTotal - rewardAmt * minusFactor) / assetTotal)
-        );
-
-        farm.lp.transferFrom(msg.sender, treasury, _lpWad);
-        exoticVesting.addVest(_for, uint112(rewardAmt));
+    constructor(
+        CZFarm _czf,
+        uint112 _baseEmissionRate,
+        address _treasury
+    ) {
+        _setupRole(EXOTIC_PRICER, _msgSender());
+        _setupRole(EXOTIC_LORD, _msgSender());
+        czf = _czf;
+        baseEmissionRate = _baseEmissionRate;
+        treasury = _treasury;
     }
 
-    function getPrice(uint256 _id) public view returns (uint256) {
-        Farm storage farm = farms[_id];
+    function getAdjustedRateBasis(uint32 _rateBasis)
+        public
+        view
+        returns (uint32 adjustedRateBasis_)
+    {
+        if (currentEmissionRate <= baseEmissionRate) return _rateBasis;
         return
-            farm.startPriceWad *
-            (((block.timestamp - farm.lastPurchaseEpoch) *
-                (10000 + hourlyIncreaseBasis)) / (1 hours * 10000));
+            uint32(
+                (uint112(_rateBasis) * baseEmissionRate) / currentEmissionRate
+            );
     }
 
-    //TODO: Add inversion of control for ExoticVestingsss
-    //TODO: add voting for allocations to farms
-    function vote(uint16[] calldata allocBasis) external view {
-        require(
-            allocBasis.length == farms.length,
-            "ExoticMaster: Must have allocation for each farm"
+    function getExoticFarmInfo(uint256 _pid)
+        external
+        view
+        returns (
+            uint32 adjustedRateBasis_,
+            uint32 vestPeriod_,
+            uint112 poolEmissionRate_,
+            uint112 czfPerAssetWad_,
+            IERC20 asset_
+        )
+    {
+        ExoticFarm storage farm = exoticFarms[_pid];
+        ChronoVesting vest = farm.chronoVesting;
+        adjustedRateBasis_ = getAdjustedRateBasis(farm.rateBasis);
+        vestPeriod_ = vest.vestPeriod();
+        poolEmissionRate_ = vest.totalEmissionRate();
+        czfPerAssetWad_ = farm.czfPerAssetWad;
+        asset_ = farm.asset;
+    }
+
+    function getExoticFarmAccountInfo(address _for, uint256 _pid)
+        external
+        view
+        returns (
+            uint256 totalVesting_,
+            uint112 emissionRate_,
+            uint32 updateEpoch_
+        )
+    {
+        ChronoVesting vest = exoticFarms[_pid].chronoVesting;
+        totalVesting_ = vest.balanceOf(_for);
+        emissionRate_ = vest.getAccountEmissionRate(_for);
+        updateEpoch_ = vest.getAccountUpdateEpoch(_for);
+    }
+
+    function addExoticFarm(
+        uint32 _vestPeriod,
+        uint32 _apr,
+        uint112 _czfPerAssetWad,
+        IERC20 _asset
+    ) external onlyRole(EXOTIC_LORD) returns (uint256 pid_) {
+        bytes memory bytecode = abi.encodePacked(
+            type(ChronoVesting).creationCode,
+            abi.encode(address(czf), 0, _vestPeriod)
         );
-        //uint256 accountVotePower = exoticVesting
+        bytes32 salt = keccak256(
+            abi.encodePacked(
+                address(czf),
+                uint32(0),
+                _vestPeriod,
+                block.timestamp
+            )
+        );
+        address chronoVesting;
+        assembly {
+            chronoVesting := create2(
+                0,
+                add(bytecode, 32),
+                mload(bytecode),
+                salt
+            )
+        }
+        pid_ = exoticFarms.length;
+        exoticFarms.push(
+            ExoticFarm({
+                czfPerAssetWad: _czfPerAssetWad,
+                rateBasis: uint32(
+                    (uint256(_apr) * uint256(_vestPeriod)) / 365 days
+                ),
+                asset: _asset,
+                chronoVesting: ChronoVesting(chronoVesting)
+            })
+        );
+        czf.grantRole(keccak256("MINTER_ROLE"), chronoVesting);
     }
 
-    //TODO: owner methods to change variables, add farms, constructor
+    function setExoticFarmApr(uint256 _pid, uint32 _apr)
+        external
+        onlyRole(EXOTIC_LORD)
+    {
+        ExoticFarm storage farm = exoticFarms[_pid];
+        ChronoVesting vest = farm.chronoVesting;
+        farm.rateBasis = uint32(
+            ((uint256(_apr) * uint256(vest.vestPeriod())) / 365 days)
+        );
+    }
+
+    function setExoticFarmAssetRate(uint256 _pid, uint112 _czfPerAssetWad)
+        external
+        onlyRole(EXOTIC_PRICER)
+    {
+        ExoticFarm storage farm = exoticFarms[_pid];
+        farm.czfPerAssetWad = _czfPerAssetWad;
+    }
+
+    function deposit(uint256 _pid, uint256 _wad) public {
+        //TODO: Fix deposits
+        /*uint256 rewardWad = (_wad *
+            (10000 + getAdjustedRateBasis(chronoPools[_pid].rateBasis))) /
+            10000;
+        czf.burnFrom(msg.sender, _wad);
+        currentEmissionRate += chronoPools[_pid].chronoVesting.addVest(
+            msg.sender,
+            uint112(rewardWad)
+        );*/
+    }
+
+    function claimAll() public {
+        for (uint256 i; i < exoticFarms.length; i++) {
+            claimFarm(i);
+        }
+    }
+
+    function claimFarm(uint256 _pid) public {
+        claimFarmForTo(msg.sender, _pid, uint32(block.timestamp));
+    }
+
+    function claimFarmForTo(
+        address _for,
+        uint256 _pid,
+        uint32 _epoch
+    ) public {
+        currentEmissionRate -= exoticFarms[_pid].chronoVesting.claimForTo(
+            _for,
+            _epoch
+        );
+    }
+
+    function setBaseEmissionRate(uint112 _to) external onlyRole(EXOTIC_LORD) {
+        baseEmissionRate = _to;
+    }
+
+    function setTreasury(address _to) external onlyRole(EXOTIC_LORD) {
+        treasury = _to;
+    }
 }
---
+++ packages/sc/contracts/Treasury.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+// Authored by Plastic Digits
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Treasury is Ownable {
+    using SafeERC20 for IERC20;
+
+    function recoverERC20(address tokenAddress) external onlyOwner {
+        IERC20(tokenAddress).safeTransfer(
+            _msgSender(),
+            IERC20(tokenAddress).balanceOf(address(this))
+        );
+    }
+}
