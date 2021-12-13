5c998b4fbc69fddcc69f0c50bddf0fbba04c5909
Plastic Digits <plasticdigits@protonmail.com>
2021-12-13 00:19:13 -0800

    Fix claimable display

--- packages/ui/src/components/Header/index.js
+++ packages/ui/src/components/Header/index.js
@@ -109,7 +109,7 @@ function Header() {
           </>
         )}
       </div>
-      <Text className="version">v0.11.3</Text>
+      <Text className="version">v0.11.4</Text>
     </header>
   );
 }
