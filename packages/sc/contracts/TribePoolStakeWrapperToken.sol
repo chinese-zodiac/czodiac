a45ce5810e5864b02664381505e1391a3c7d176d
Plastic Digits <plasticdigits@protonmail.com>
2022-11-01 04:46:50 -0700

    Change tribe pool wrapper to include nft slotting, withdraw fee, and token whitelisting for actions.

--- packages/sc/contracts/TribePoolStakeWrapperToken.sol
+++ packages/sc/contracts/TribePoolStakeWrapperToken.sol
@@ -17,21 +17,66 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "./CZFarm.sol";
 import "./TribePool.sol";
 
-contract TribePoolStakeWrapperToken is Context, ERC20Wrapper, Ownable {
+contract TribePoolStakeWrapperToken is
+    Context,
+    ERC20Wrapper,
+    Ownable,
+    ReentrancyGuard
+{
     using SafeERC20 for IERC20;
 
+    struct SlottedNft {
+        uint256 id;
+        uint256 timestamp;
+    }
+
+    CZFarm public czf = CZFarm(0x7c1608C004F20c3520f70b924E2BfeF092dA0043);
+
     TribePool public pool;
+    mapping(address => mapping(IERC721 => SlottedNft)) accountSlottedNfts;
+
+    //slot this NFT to remove fees
+    IERC721 public slottableNftTaxFree;
+    //Nft unlock period
+    uint256 public nftLockPeriod;
+    // Whitelist token
+    IERC20 public whitelistToken;
+    // Whitelist token wad required
+    uint256 public whitelistWad;
+    //withdraw fee in basis points (0.01%)
+    uint256 public withdrawFeeBasis;
+
+    modifier onlyWhitelist() {
+        if (
+            whitelistWad != 0 &&
+            whitelistToken != IERC20(address(0x0)) &&
+            msg.sender != owner()
+        ) {
+            require(
+                whitelistToken.balanceOf(msg.sender) >= whitelistWad,
+                "Not enough whitelist token to deposit"
+            );
+        }
+        _;
+    }
 
     constructor(
         string memory _name,
         string memory _symbol,
-        address _underlying, //should be czf
-        address _pool //will issue claimable rewards to holders
-    ) ERC20(_name, _symbol) ERC20Wrapper(IERC20(_underlying)) {}
+        address _pool, //will issue claimable rewards to holders
+        uint256 _withdrawFeeBasis,
+        IERC20 _whitelistToken,
+        uint256 _whitelistWad,
+        IERC721 _slottableNftTaxFree,
+        uint256 _nftLockPeriod
+    ) ERC20(_name, _symbol) ERC20Wrapper(IERC20(czf)) Ownable() {}
 
     function _beforeTokenTransfer(
         address from,
@@ -43,7 +88,96 @@ contract TribePoolStakeWrapperToken is Context, ERC20Wrapper, Ownable {
         pool.deposit(to, amount);
     }
 
+    function depositFor(address _account, uint256 _amount)
+        public
+        override
+        onlyWhitelist
+        returns (bool)
+    {
+        super.depositFor(_account, _amount);
+        return true;
+    }
+
+    function withdrawTo(address _account, uint256 _amount)
+        public
+        override
+        onlyWhitelist
+        returns (bool)
+    {
+        uint256 withdrawFee = (accountSlottedNfts[_account][slottableNftTaxFree]
+            .timestamp != 0)
+            ? 0
+            : ((_amount * withdrawFeeBasis) / 10000);
+        if (withdrawFee > 0) czf.burn(withdrawFee);
+        super.withdrawTo(_account, _amount - withdrawFee);
+        return true;
+    }
+
     function setPool(TribePool _to) external onlyOwner {
         pool = _to;
+    }
+
+    function getSlottedNft(address _account, IERC721 _nftSc)
+        external
+        view
+        returns (uint256 id_, uint256 timestamp_)
+    {
+        SlottedNft storage slottedNft = accountSlottedNfts[_account][_nftSc];
+        id_ = slottedNft.id;
+        timestamp_ = slottedNft.timestamp;
+    }
+
+    function slotNft(IERC721 _nftSc, uint256 _nftId) public nonReentrant {
+        SlottedNft storage slottedNft = accountSlottedNfts[msg.sender][_nftSc];
+        require(slottedNft.timestamp == 0, "CZ: NFT already slotted");
+        slottedNft.id = _nftId;
+        slottedNft.timestamp = block.timestamp;
+        _nftSc.transferFrom(msg.sender, address(this), _nftId);
+    }
+
+    function unslotNft(IERC721 _nftSc) public nonReentrant {
+        SlottedNft storage slottedNft = accountSlottedNfts[msg.sender][_nftSc];
+        require(slottedNft.timestamp != 0, "CZ: No NFT Slotted");
+        require(
+            block.timestamp > slottedNft.timestamp + nftLockPeriod,
+            "CZ: NFT Locked"
+        );
+        _nftSc.transferFrom(address(this), msg.sender, slottedNft.id);
+        delete slottedNft.id;
+        delete slottedNft.timestamp;
+        delete accountSlottedNfts[msg.sender][_nftSc];
+    }
+
+    function setWithdrawFeeBasis(uint256 _withdrawFeeBasis) public onlyOwner {
+        withdrawFeeBasis = _withdrawFeeBasis;
+    }
+
+    function setWhitelistToken(IERC20 _whitelistToken) public onlyOwner {
+        whitelistToken = _whitelistToken;
+    }
+
+    function setWhitelistWad(uint256 _whitelistWad) public onlyOwner {
+        whitelistWad = _whitelistWad;
+    }
+
+    function setSlottableNftTaxFree(IERC721 _slottableNftTaxFree)
+        public
+        onlyOwner
+    {
+        slottableNftTaxFree = _slottableNftTaxFree;
+    }
+
+    function setNftLockPeriod(uint256 _nftLockPeriod) public onlyOwner {
+        nftLockPeriod = _nftLockPeriod;
+    }
+
+    function recoverWrongTokens(
+        address _tokenAddress,
+        uint256 _tokenAmount,
+        address _to
+    ) external onlyOwner {
+        require(_tokenAddress != address(underlying), "Cannot be underlying");
+
+        IERC20(_tokenAddress).safeTransfer(_to, _tokenAmount);
     }
 }
