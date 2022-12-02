6de298f87994c5d51f8e3cbb7bac062707e8168b
Plastic Digits <plasticdigits@protonmail.com>
2022-12-02 00:06:27 -0800

    Update TribePools for CZRed

--- packages/sc/contracts/TribePoolMaster.sol
+++ packages/sc/contracts/TribePoolMaster.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "./TribePool.sol";
 import "./TribePoolStakeWrapperToken.sol";
 import "./CZUsd.sol";
-import "./CZFarm.sol";
+import "./CZRed.sol";
 import "./libs/IterableArrayWithoutDuplicateKeys.sol";
 
 //import "hardhat/console.sol";
@@ -20,51 +20,26 @@ contract TribePoolMaster is AccessControlEnumerable {
     bytes32 public constant MANAGER_SETTINGS = keccak256("MANAGER_SETTINGS");
     bytes32 public constant MANAGER_POOLS = keccak256("MANAGER_POOLS");
 
-    uint256 public czusdPerSecond;
-
     CZUsd public czusd = CZUsd(0xE68b79e51bf826534Ff37AA9CeE71a3842ee9c70);
-    CZFarm public czf = CZFarm(0x7c1608C004F20c3520f70b924E2BfeF092dA0043);
+    CZRed public czr = CZRed(0x5cd0c2C744caF04cda258Efc6558A3Ed3defE97b);
 
     IterableArrayWithoutDuplicateKeys.Map tribePools;
     mapping(address => uint256) public weights;
     uint256 public totalWeight;
 
-    uint256 public lastUpdate;
-
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        lastUpdate = block.timestamp;
     }
 
-    function setCzusdPerSecond(uint256 _to)
-        external
-        onlyRole(MANAGER_SETTINGS)
-    {
-        updatePools();
-        lastUpdate = block.timestamp;
-        czusdPerSecond = _to;
-    }
-
-    function updatePools() public {
-        if (lastUpdate == block.timestamp) return;
-        if (czusdPerSecond == 0) {
-            lastUpdate = block.timestamp;
-            return;
-        }
-
-        czusd.mint(
-            address(this),
-            czusdPerSecond * (block.timestamp - lastUpdate)
-        );
+    function addRewardsWithCzusd(uint256 _czusdWad) external {
+        czusd.transferFrom(msg.sender, address(this), _czusdWad);
         for (uint256 i = 0; i < tribePools.size(); i++) {
             address pool = tribePools.getKeyAtIndex(i);
             if (weights[pool] != 0) {
-                uint256 rewardsWad = (((block.timestamp - lastUpdate) *
-                    weights[pool]) * czusdPerSecond) / totalWeight;
-                TribePool(pool).addRewards(rewardsWad);
+                uint256 rewardsWad = (_czusdWad * weights[pool]) / totalWeight;
+                TribePool(pool).addRewardsWithCzusd(rewardsWad);
             }
         }
-        lastUpdate = block.timestamp;
     }
 
     function getIsTribePool(address _address) public view returns (bool) {
@@ -85,7 +60,6 @@ contract TribePoolMaster is AccessControlEnumerable {
         uint256 _weight,
         address _owner
     ) public onlyRole(MANAGER_POOLS) {
-        updatePools();
         TribePoolStakeWrapperToken poolWrapper = new TribePoolStakeWrapperToken(
             string(abi.encodePacked("CZF Staked in ", _tribeToken.name())), //string memory _name,
             string(abi.encodePacked("cz-", _tribeToken.symbol())), //string memory _symbol,
@@ -94,7 +68,7 @@ contract TribePoolMaster is AccessControlEnumerable {
             _owner
         );
         address newPool = address(poolWrapper.pool());
-        czf.setContractSafe(address(poolWrapper));
+        czr.setContractSafe(address(poolWrapper));
         czusd.setContractSafe(newPool);
         tribePools.add(newPool);
         weights[newPool] = _weight;
@@ -102,7 +76,6 @@ contract TribePoolMaster is AccessControlEnumerable {
     }
 
     function removeTribePool(address _pool) external onlyRole(MANAGER_POOLS) {
-        updatePools();
         totalWeight -= weights[_pool];
         weights[_pool] = 0;
         tribePools.remove(_pool);
@@ -116,7 +89,6 @@ contract TribePoolMaster is AccessControlEnumerable {
             getIsTribePool(_pool) == true,
             "TribePoolMaster: Not tribe pool"
         );
-        updatePools();
         totalWeight -= weights[_pool];
         weights[_pool] = _weight;
         totalWeight += _weight;
