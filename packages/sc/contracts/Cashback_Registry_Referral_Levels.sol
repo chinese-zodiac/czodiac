bae59b80f5042c2de585a376965cf30f9872d9df
Plastic Digits <plasticdigits@protonmail.com>
2023-02-11 07:40:49 -0800

    Update cashback registry referral levels lookup

--- packages/sc/contracts/Cashback_Registry_Referral_Levels.sol
+++ packages/sc/contracts/Cashback_Registry_Referral_Levels.sol
@@ -19,17 +19,46 @@ contract Cashback_Registry_Referral_Levels is Ownable {
         external
         view
         returns (
-            uint64[] memory accountIds,
-            Cashback_Registry.LEVEL[] memory levels
+            uint64[] memory accountIds_,
+            Cashback_Registry.LEVEL[] memory levels_,
+            bool[] memory canRecaptures_
         )
     {
+        levels_ = new Cashback_Registry.LEVEL[](_count);
+        canRecaptures_ = new bool[](_count);
         uint64 signerAccountId = CBR.signerToAccountId(_signer);
-        accountIds = CBR.getAccountReferrals(signerAccountId, _start, _count);
+        (
+            Cashback_Registry.LEVEL signerLevel,
+            ,
+            uint64[6] memory signerLevelNodeIds,
+            ,
+            ,
+            ,
+            ,
+
+        ) = CBR.getAccountInfo(signerAccountId);
+        accountIds_ = CBR.getAccountReferrals(signerAccountId, _start, _count);
         for (uint256 i = 0; i < _count; i++) {
-            (Cashback_Registry.LEVEL level, , , , , , , ) = CBR.getSignerInfo(
-                _signer
-            );
-            levels[i] = level;
+            (
+                Cashback_Registry.LEVEL level,
+                ,
+                uint64[6] memory accountLevelNodeIds,
+                ,
+                ,
+                ,
+                ,
+
+            ) = CBR.getAccountInfo(accountIds_[i]);
+            levels_[i] = level;
+            if (level <= signerLevel || uint8(level) == 5) {
+                canRecaptures_[i] = false;
+            } else {
+                (, , uint64 accountParentId) = CBR.getNodeInfo(
+                    accountLevelNodeIds[uint256(level)]
+                );
+                canRecaptures_[i] =
+                    signerLevelNodeIds[uint256(level) - 1] != accountParentId;
+            }
         }
     }
 
