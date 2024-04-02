f4a0bff134b0ee65a671e89db5964ed1dd22f6c8
Plastic Digits <plasticdigits@protonmail.com>
2024-04-03 08:27:24 +1100

    Update lsdt airdrop script. Fix bug in yarn.lock for concat-stream

--- packages/sc/scripts/lsdt-airdrop.js
+++ packages/sc/scripts/lsdt-airdrop.js
@@ -1,6 +1,12 @@
 const hre = require("hardhat");
 const loadJsonFile = require("load-json-file");
-const { lsdt, SilverDollarNfts, CzUstsdReserves, Gangs, EntityStoreERC721 } = loadJsonFile.sync("./deployConfig.json");
+const {
+  lsdt,
+  SilverDollarNfts,
+  CzUstsdReserves,
+  Gangs,
+  EntityStoreERC721,
+} = loadJsonFile.sync("./deployConfig.json");
 const lsdtAirdropAddr = "0x95a3a3b824b0D7aBfFBfe48Bc80F2b56616D441d";
 
 const { ethers } = hre;
@@ -9,9 +15,15 @@ const { parseEther, formatEther } = ethers.utils;
 async function main() {
   const lsdtSc = await ethers.getContractAt("LSDT", lsdt);
   const airdropSc = await ethers.getContractAt("LSDTRewards", lsdtAirdropAddr);
-  const ustsdSc = await ethers.getContractAt("JsonNftTemplate", SilverDollarNfts);
+  const ustsdSc = await ethers.getContractAt(
+    "JsonNftTemplate",
+    SilverDollarNfts
+  );
   const gangsSc = await ethers.getContractAt("IERC721Enumerable", Gangs);
-  const erc721EnumberableSc = await ethers.getContractAt("EntityStoreERC721", EntityStoreERC721);
+  const erc721EnumberableSc = await ethers.getContractAt(
+    "EntityStoreERC721",
+    EntityStoreERC721
+  );
 
   const lsdtToAirdrop = await lsdtSc.balanceOf(lsdtAirdropAddr);
 
@@ -19,16 +31,24 @@ async function main() {
   let totalStrength = 0;
   const winners = [];
   const winnersToPick = Math.floor(Number(formatEther(lsdtToAirdrop)) / 5);
-  for (let i = 0; i < await gangsSc.totalSupply(); i++) {
+  for (let i = 0; i < (await gangsSc.totalSupply()); i++) {
     const gangOwner = await gangsSc.ownerOf(i);
-    const gangUstsdCount = Number((await erc721EnumberableSc.getStoredERC721CountFor(Gangs, i, SilverDollarNfts)).toString())
+    const gangUstsdCount = Number(
+      (
+        await erc721EnumberableSc.getStoredERC721CountFor(
+          Gangs,
+          i,
+          SilverDollarNfts
+        )
+      ).toString()
+    );
     totalStrength += gangUstsdCount;
     let cutoff = totalStrength;
     gangStats.push({
       gangOwner,
       gangUstsdCount,
       cutoff,
-      gangId: i
+      gangId: i,
     });
     console.log(i, gangOwner, gangUstsdCount, cutoff);
   }
@@ -38,12 +58,14 @@ async function main() {
     let winnerGangId;
     for (let j = 0; j < gangStats.length; j++) {
       if (gangStats[j].cutoff < roll) {
-        winnerGangId = gangStats[j].gangId;
+        winnerGangId = gangStats[j].gangId + 1;
       } else {
         console.log("found winner:", winnerGangId);
-        winners.push(winnerGangId);
+        winners.push(
+          winnerGangId
+        ); /*
         await airdropSc.sendAirdrop(winnerGangId, parseEther("5"))
-        await delay(10000);
+        await delay(10000);*/
         break;
       }
     }
@@ -67,14 +89,14 @@ function shuffleArray(array) {
 }
 
 function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
